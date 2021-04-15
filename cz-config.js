a352f529d232bf90b033f181b0bf37247e178299
Joe Maddalone <joe.maddalone@gmail.com>
2021-04-15 07:24:58 -0500

    refactor(cleanup) clear out mock files
    Summary:
    
    Test Plan:
    see files are removed :)
    
    Ref: REF-5555
    
    Semver: patch

--- cz-config.js
+++ cz-config.js
@@ -1,78 +1,113 @@
-module.exports = {
-  types: [
-    { value: "feat", name: "feat:     A new feature" },
-    { value: "fix", name: "fix:      A bug fix" },
-    { value: "docs", name: "docs:     Documentation only changes" },
-    {
-      value: "style",
-      name:
-        "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
-    },
-    {
-      value: "refactor",
-      name:
-        "refactor: A code change that neither fixes a bug nor adds a feature",
-    },
-    {
-      value: "perf",
-      name: "perf:     A code change that improves performance",
-    },
-    { value: "test", name: "test:     Adding missing tests" },
-    {
-      value: "chore",
-      name:
-        "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
-    },
-    { value: "revert", name: "revert:   Revert to a commit" },
-    { value: "WIP", name: "WIP:      Work in progress" },
-  ],
+"use strict";
 
-  scopes: [
-    { name: "views" },
-    { name: "boards" },
-    { name: "screens" },
-    { name: "settings" },
-  ],
-
-  allowTicketNumber: false,
-  isTicketNumberRequired: true,
-  ticketNumberPrefix: "LOG-",
-  ticketNumberRegExp: "\\d{1,5}",
-  footerPrefix: "Ref:",
-
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-	scopeOverrides: {
-	  fix: [
-		{name: 'merge'},
-		{name: 'style'},
-		{name: 'e2eTest'},
-		{name: 'unitTest'}
-	  ]
-	},
-	*/
-  // override the messages, defaults are as follows
-  messages: {
-    type: "Select the type of change that you're committing:",
-    scope: "\nSet the SCOPE of this change (optional):",
-    // used if allowCustomScopes is true
-    customScope: "Set the SCOPE of this change:",
-    subject: "Write a SHORT, IMPERATIVE description of the change:\n",
-    body:
-      'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: "List any BREAKING CHANGES (optional):\n",
-    footer:
-      "Jira ticket for this change (optional). E.g.: LOG-34:\n",
-    confirmCommit: "Are you sure you want to proceed with the commit above?",
+const questions = [
+  {
+    type: "list",
+    name: "type",
+    message: "type",
+    choices: [
+      "feat     (new feature)",
+      "fix      (bug fix)",
+      "refactor (refactoring production code)",
+      "style    (formatting, missing semi colons, etc; no code change)",
+      "bump     (update version of repo or package)",
+      "docs     (changes to documentation)",
+      "test     (adding or refactoring tests; no production code change)",
+      "chore    (updating grunt tasks etc; no production code change)",
+    ],
+    filter: function (val) {
+      return val.toLowerCase();
+    },
   },
+  {
+    type: "input",
+    name: "scope",
+    message: "scope (optional)",
+    validate: function (text) {
+      if (text.length > 100) {
+        return "test too long";
+      }
 
-  allowCustomScopes: true,
-  allowBreakingChanges: ["feat", "fix"],
-  // skip any questions you want
-//   skipQuestions: ["body"],
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "semver",
+    message: "Semver",
+    choices: [
+      "major    (when you make incompatible API changes)",
+      "minor    (when you add functionality in a backwards-compatible manner)",
+      "patch    (when you make backwards-compatible bug fixes)",
+    ],
+  },
+  {
+    type: "input",
+    name: "subject",
+    message: "commit message (72 chars max.)",
+    validate: function (text) {
+      if (text.length > 72) {
+        const over = text.length - 72;
+        return `Over by ${over} characters. :(`;
+      }
 
-  // limit subject length
-  subjectLimit: 100,
-  // breaklineChar: '|', // It is supported for fields body and footer.
-    // askForBreakingChangeFirst : true, // default is false
-};
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "ref",
+    message: "Jira ticket number (ex, LOG-1234)",
+    validate: function (text) {
+      if (!text) {
+        return "Ref is required, if you don't have one please enter n/a";
+      }
+
+      return true;
+    },
+  },
+  {
+    type: "confirm",
+    name: "addSummary",
+    message: "Would you like to add a summary?",
+  },
+  {
+    type: "editor",
+    name: "summary",
+    message: "Summary",
+    when: function (answers) {
+      return answers.addSummary;
+    },
+  },
+  {
+    type: "editor",
+    name: "testplan",
+    message: "Test plan",
+    validate: function (text) {
+      if (!text) {
+        return "Test plan is required";
+      }
+
+      return true;
+    },
+  },
+];
+
+module.exports = {
+  prompter: (cz, commit) => {
+    cz.prompt(questions).then((answers) => {
+      commit(`${answers.type.split(" ")[0]}${
+answers.scope ? `(${answers.scope})` : ""
+} ${answers.subject}
+
+Summary: ${answers.summary ? answers.summary : ''}
+
+Test Plan:
+${answers.testplan}
+
+Ref: ${answers.ref}
+
+Semver: ${answers.semver.split(" ")[0]}`);
+    });
+  }
+}
